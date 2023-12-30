d5ac61887fc53a0ac7978f7de03d41e09b447959
Shantanu Taro <shantanutaro@gmail.com>
2023-12-30 17:33:41 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,156 +1,52 @@
-const express = require('express');
-const cookieParser = require("cookie-parser");
-const sessions = require('express-session');
-const http = require('http');
-var parseUrl = require('body-parser');
-const app = express();
+let express=require("express");
+let bodyParser=require("body-parser");
 
-var mysql = require('mysql');
-const { encode } = require('punycode');
-
-let encodeUrl = parseUrl.urlencoded({ extended: false });
-
-//session middleware
-app.use(sessions({
-    secret: "thisismysecrctekey",
-    saveUninitialized:true,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 24 hours
-    resave: false
-}));
-
-app.use(cookieParser());
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root", // my username
-    password: "123456789", // my password
-    database: "myform"
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/register.html');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/ChargingDock');
+let db=mongoose.connection;
+db.on('error', console.log.bind(console, "connection error"));
+db.once('open', function(callback){
+	console.log("connection succeeded");
 })
 
-app.post('/register', encodeUrl, (req, res) => {
-    var firstName = req.body.firstName;
-    var lastName = req.body.lastName;
-    var userName = req.body.userName;
-    var password = req.body.password;
+let app=express()
 
-    con.connect(function(err) {
-        if (err){
-            console.log(err);
-        };
-        // checking user already registered or no
-        con.query(`SELECT * FROM users WHERE username = '${userName}' AND password  = '${password}'`, function(err, result){
-            if(err){
-                console.log(err);
-            };
-            if(Object.keys(result).length > 0){
-                res.sendFile(__dirname + '/failReg.html');
-            }else{
-            //creating user page in userPage function
-            function userPage(){
-                // We create a session for the dashboard (user page) page and save the user data to this session:
-                req.session.user = {
-                    firstname: firstName,
-                    lastname: lastName,
-                    username: userName,
-                    password: password 
-                };
 
-                res.send(`
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <title>Login and register form with Node.js, Express.js and MySQL</title>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1">
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-                </head>
-                <body>
-                    <div class="container">
-                        <h3>Hi, ${req.session.user.firstname} ${req.session.user.lastname}</h3>
-                        <a href="/">Log out</a>
-                    </div>
-                </body>
-                </html>
-                `);
-            }
-                // inserting new user data
-                var sql = `INSERT INTO users (firstname, lastname, username, password) VALUES ('${firstName}', '${lastName}', '${userName}', '${password}')`;
-                con.query(sql, function (err, result) {
-                    if (err){
-                        console.log(err);
-                    }else{
-                        // using userPage function for creating user page
-                        userPage();
-                    };
-                });
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
+app.post('/register', function(req,res){
+ 	    let firstName = req.body.firstName;
+        let lastName =req.body.lastName;
+        let userName = req.body.userName;
+        let password =req.body.password;
+
+        let data = {
+            "firstName": firstName,
+            "lastName":lastName,
+            "userName":userName,
+            "password":password
         }
-
+        console.log(data)
+        db.collection('registration').insertOne(data,function(err, collection){
+            if (err) throw err;
+            console.log("Record inserted Successfully");
+                 
         });
-    });
+             
+        return res.redirect('signup_success.html');
+})
 
 
-});
+app.get('/',function(req,res){
+res.set({
+	'Access-control-Allow-Origin': '*'
+	});
+return res.redirect('index.html');
+}).listen(3000)
 
-app.get("/login", (req, res)=>{
-    res.sendFile(__dirname + "/login.html");
-});
 
-app.post("/dashboard", encodeUrl, (req, res)=>{
-    var userName = req.body.userName;
-    var password = req.body.password;
-
-    con.connect(function(err) {
-        if(err){
-            console.log(err);
-        };
-        con.query(`SELECT * FROM users WHERE username = '${userName}' AND password = '${password}'`, function (err, result) {
-          if(err){
-            console.log(err);
-          };
-
-          function userPage(){
-            // We create a session for the dashboard (user page) page and save the user data to this session:
-            req.session.user = {
-                firstname: result[0].firstname,
-                lastname: result[0].lastname,
-                username: userName,
-                password: password 
-            };
-
-            res.send(`
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <title>Login and register form with Node.js, Express.js and MySQL</title>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-            </head>
-            <body>
-                <div class="container">
-                    <h3>Hi, ${req.session.user.firstname} ${req.session.user.lastname}</h3>
-                    <a href="/">Log out</a>
-                </div>
-            </body>
-            </html>
-            `);
-        }
-
-        if(Object.keys(result).length > 0){
-            userPage();
-        }else{
-            res.sendFile(__dirname + '/failLog.html');
-        }
-
-        });
-    });
-});
-
-app.listen(4000, ()=>{
-    console.log("Server running on port 4000");
-});
+console.log("server listening at port 3000");
