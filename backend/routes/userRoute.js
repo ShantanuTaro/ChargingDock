ce6bb296074a5eb6e79dadfdf317d95afb71c834
Shantanu Taro <shantanutaro@gmail.com>
2024-01-03 21:48:16 +0530

    Updated the code with new naming convention

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const userController = require("../controllers/userController")
+const customerController = require("../controllers/customerController")
 
-router.get('/user',userController.index)
-router.post('/register',userController.register)
-router.post('/login',userController.login)
+router.get('/listCustomers',customerController.listCustomers)
+router.post('/customerRegistration',customerController.customerRegistration)
+router.post('/customerLogin',customerController.customerLogin)
 
 
 module.exports = router
